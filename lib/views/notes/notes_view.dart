dceb5b4f335b1160eac58ea7f3d25847358feea0
rohk30 <134207791+rohk30@users.noreply.github.com>
2025-04-12 14:34:07 +0530

    Step-30_Added the dynamic search functionality

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,4 +1,5 @@
 // import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rohnewnotes/services/auth/auth_service.dart';
@@ -31,41 +32,19 @@ class _NotesViewState extends State<NotesView> {
     super.initState();
   }
 
-  void _openSearchDialog() async {
-    final query = await showDialog<String>(
+  void _openSearchDialog() {
+    showDialog(
       context: context,
       builder: (context) {
-        String tempQuery = '';
-        return AlertDialog(
-          title: const Text('Search Notes'),
-          content: TextField(
-            autofocus: true,
-            decoration: const InputDecoration(hintText: 'Enter title...'),
-            onChanged: (value) {
-              tempQuery = value;
-            },
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(tempQuery);
-              },
-              child: const Text('Search'),
-            ),
-          ],
+        return _LiveSearchDialog(
+          onQueryChanged: (query) {
+            setState(() {
+              _searchQuery = query.trim().toLowerCase();
+            });
+          },
         );
       },
     );
-
-    if (query != null) {
-      setState(() {
-        _searchQuery = query.trim().toLowerCase();
-      });
-    }
   }
 
   @override
@@ -158,314 +137,51 @@ class _NotesViewState extends State<NotesView> {
   }
 }
 
+class _LiveSearchDialog extends StatefulWidget {
+  final void Function(String query) onQueryChanged;
 
-/*
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
+  const _LiveSearchDialog({required this.onQueryChanged});
 
   @override
-  State<NotesView> createState() => _NotesViewState();
+  State<_LiveSearchDialog> createState() => _LiveSearchDialogState();
 }
 
-class _NotesViewState extends State<NotesView> {
-  late final FirebaseCloudStorage _notesService;
-  String get userId => AuthService.firebase().currentUser!.id;
+class _LiveSearchDialogState extends State<_LiveSearchDialog> {
+  late TextEditingController _controller;
 
   @override
   void initState() {
-    _notesService = FirebaseCloudStorage();
+    _controller = TextEditingController();
+    _controller.addListener(() {
+      widget.onQueryChanged(_controller.text);
+    });
     super.initState();
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Notes'),
-        backgroundColor: Colors.lightBlue,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
-            },
-            icon: const Icon(Icons.add),
-          ),
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch (value) {
-                case MenuAction.logout:
-                  final shouldLogout = await showLogOutDialog(context);
-                  if (shouldLogout) {
-                    context.read<AuthBloc>().add(const AuthEventLogOut());
-                  }
-              }
-            },
-            itemBuilder: (context) {
-              return const [
-                PopupMenuItem<MenuAction>(
-                  value: MenuAction.logout,
-                  child: Text('Log out'),
-                ),
-              ];
-            },
-          )
-        ],
+    return AlertDialog(
+      title: const Text('Search Notes'),
+      content: TextField(
+        controller: _controller,
+        autofocus: true,
+        decoration: const InputDecoration(hintText: 'Type to search...'),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blueAccent, Colors.lightBlue],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: StreamBuilder<Iterable<CloudNote>>(
-          stream: _notesService.allNotes(ownerUserId: userId),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-              case ConnectionState.active:
-                if (snapshot.hasData) {
-                  final allNotes = snapshot.data!;
-                  return NotesListView(
-                    notes: allNotes,
-                    onDeleteNote: (note) async {
-                      await _notesService.deleteNote(documentId: note.documentId);
-                    },
-                    onTap: (note) {
-                      Navigator.of(context).pushNamed(
-                        createOrUpdateNoteRoute,
-                        arguments: note,
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              default:
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-            }
+      actions: [
+        TextButton(
+          onPressed: () {
+            widget.onQueryChanged('');
+            Navigator.of(context).pop();
           },
+          child: const Text('Close'),
         ),
-      ),
-    );
-  }
-}     */
-
-
-/*
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
-
-  @override
-  State<NotesView> createState() => _NotesViewState();
-}
-
-class _NotesViewState extends State<NotesView> {
-  late final FirebaseCloudStorage _notesService;
-  String get userId => AuthService.firebase().currentUser!.id;
-
-  @override
-  void initState() {
-    _notesService = FirebaseCloudStorage();
-    super.initState();
-  }
-
-  void _navigateToCreateNote() {
-    Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
-  }
-
-  void _navigateToNote(CloudNote note) {
-    Navigator.of(context).pushNamed(
-      createOrUpdateNoteRoute,
-      arguments: note,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Notes'),
-        actions: [
-          IconButton(
-            onPressed: _navigateToCreateNote,
-            icon: const Icon(Icons.add),
-          ),
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              if (value == MenuAction.logout) {
-                final shouldLogout = await showLogOutDialog(context);
-                if (shouldLogout) {
-                  await AuthService.firebase().logOut();
-                  if (mounted) {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      loginRoute,
-                          (_) => false,
-                    );
-                  }
-                }
-              }
-            },
-            itemBuilder: (context) => [
-              const PopupMenuItem<MenuAction>(
-                value: MenuAction.logout,
-                child: Text('Log Out'),
-              ),
-            ],
-          )
-        ],
-      ),
-      body: StreamBuilder(
-        stream: _notesService.allNotes(ownerUserId: userId),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              if (snapshot.hasData) {
-                final allNotes = snapshot.data as Iterable<CloudNote>;
-                return NotesListView(
-                  notes: allNotes,
-                  onDeleteNote: (note) async {
-                    await _notesService.deleteNote(documentId: note.documentId);
-                  },
-                  onTap: _navigateToNote,
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            default:
-              return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
-  }
-}     */
-
-
-/*
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
-
-  @override
-  State<NotesView> createState() => _NotesViewState();
-}
-
-class _NotesViewState extends State<NotesView> {
-
-  late final FirebaseCloudStorage _notesService;
-  // String get userId => AuthService.firebase().currentUser!.id;
-  String get userId {
-    final user = AuthService.firebase().currentUser;
-    if (user == null) {
-      Navigator.of(context).pushNamedAndRemoveUntil(
-        loginRoute,
-            (route) => false,
-      );
-      return '';
-    }
-    return user.id;
-  }
-
-  @override
-  void initState() {
-    _notesService = FirebaseCloudStorage();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your notes'),
-        actions: [
-          IconButton(onPressed: () {
-            Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
-          }, icon: const Icon(Icons.add)),
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch (value) {
-                case MenuAction.logout:
-                  final shouldLogout = await showLogOutDialog(context);
-                  // devtools.log(shouldLogout.toString());
-                  if (shouldLogout) {
-                    context.read<AuthBloc>().add(
-                        const AuthEventLogOut()
-                    );
-                  }
-                  break;
-              }
-            },
-            itemBuilder: (context) {
-              return const [
-                PopupMenuItem<MenuAction>(
-                    value: MenuAction.logout, child: Text('Log out'))
-              ];
-            },
-          )
-        ],
-        // backgroundColor: Colors.blue,
-      ),
-      body: StreamBuilder(
-        stream: _notesService.allNotes(ownerUserId: userId),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              if (snapshot.hasData) {
-                final allNotes = snapshot.data as Iterable<CloudNote>;
-                return NotesListView(
-                  notes: allNotes,
-                  onDeleteNote: (note) async {
-                    await _notesService.deleteNote(documentId: note.documentId);
-                  },
-                  onTap: (note) {
-                    Navigator.of(context).pushNamed(
-                        createOrUpdateNoteRoute,
-                        arguments: note
-                    );
-                  },
-                );
-              } else {
-                return const CircularProgressIndicator();
-              }
-            default:
-              return const CircularProgressIndicator();
-          }
-        },
-      ),
+      ],
     );
   }
 }
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure you want to sign out?'),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
-            child: const Text('Log Out'),
-          ),
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
-}       */
-
